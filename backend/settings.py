f442982748dab578721b09e54122e43db7f62dcb
Louis <18758398381@163.com>
2025-06-04 21:06:13 +0800

    add auth

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
+AUTH_USER_MODEL= 'userauths.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
